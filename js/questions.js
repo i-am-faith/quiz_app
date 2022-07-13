cdb2b806815d172dea711fcb9c35385901ef1e7f
Sourin Mukherjee <98275372+i-am-faith@users.noreply.github.com>
2022-07-13 14:28:48 +0530

    Added JS File #1 ✔️

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which of the following data type stores longest decimal number ?",
+    answer: "Double",
+    options: [
+      "Long",
+      "Float",
+      "Double",
+      "Integer"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Who developed Python Programming Language?",
+    answer: "Guido van Rossum",
+    options: [
+      "Wick van Rossum",
+      "Rasmus Lerdorf",
+      "Guido van Rossum",
+      "Niene Stom"
+    ]
+  },
+  {
+    numb: 5,
+    question: "In which of the following is toString() method defined?",
+    answer: "java.lang.Object",
+    options: [
+      "java.lang.Object",
+      "java.lang.String",
+      "java.lang.util",
+      "None"
+    ]
+  },
+];
